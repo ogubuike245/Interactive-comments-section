5678d41528605d36175767ac0211e6811334a423
ogubuike <68479614+ogubuike245@users.noreply.github.com>
2023-07-20 00:08:51 +0100

    first level comments sorting

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,4 +1,46 @@
+// import Comment from "./Comment";
+
+// // Comments.jsx
+// const Comments = ({
+//   // DATA
+//   user,
+//   commentsArray,
+
+//   // ACTIONS
+//   setComments,
+//   handleAddReply,
+//   handleEditComment,
+//   handleDeleteComment,
+//   handleVoting,
+// }) => {
+//   return (
+//     // LOOP THROUGH COMMENTS
+//     <div className="comments-section">
+//       {commentsArray?.map((comment) => (
+//         <Comment
+//           key={comment.id}
+//           // DATA
+//           comment={comment}
+//           currentUser={user}
+//           comments={commentsArray}
+//           // ACTIONS
+//           setComments={setComments}
+//           handleAddReply={handleAddReply}
+//           handleEditComment={handleEditComment}
+//           handleDeleteComment={handleDeleteComment}
+//           handleVoting={handleVoting}
+//         />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Comments;
+
+import { useState, useEffect } from "react";
 import Comment from "./Comment";
+
+// ... (Your other imports)
 
 // Comments.jsx
 const Comments = ({
@@ -13,10 +55,39 @@ const Comments = ({
   handleDeleteComment,
   handleVoting,
 }) => {
+  const [sortedComments, setSortedComments] = useState([]);
+
+  useEffect(() => {
+    // Sort the comments by score in descending order (first-level comments)
+    const sortedFirstLevelComments = commentsArray
+      .filter((comment) => !comment.replyingTo)
+      .sort((a, b) => b.score - a.score);
+
+    // Sort the replies by time added (nested replies)
+    const sortedReplies = commentsArray
+      .filter((comment) => comment.replyingTo)
+      .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+
+    // Merge the sorted first-level comments and sorted replies
+    const mergedSortedComments = sortedFirstLevelComments.reduce(
+      (result, comment) => {
+        result.push(comment);
+        const commentReplies = sortedReplies.filter(
+          (reply) => reply.replyingTo === comment.id
+        );
+        result.push(...commentReplies);
+        return result;
+      },
+      []
+    );
+
+    setSortedComments(mergedSortedComments);
+  }, []);
+
   return (
     // LOOP THROUGH COMMENTS
     <div className="comments-section">
-      {commentsArray?.map((comment) => (
+      {sortedComments?.map((comment) => (
         <Comment
           key={comment.id}
           // DATA
